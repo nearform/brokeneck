0235dba0dff03c544ec64cc2e0b70f23746436ed
Simone Busoli <simone.busoli@nearform.com>
2020-12-03 12:48:35 +0100

    build: rollup

---
+++ packages/brokeneck-react/rollup.config.js
@@ -0,0 +1,33 @@
+import babel from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonJs from '@rollup/plugin-commonjs'
+import peerDeps from 'rollup-plugin-peer-deps-external'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'default'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    nodeResolve(),
+    peerDeps({
+      includeDependencies: true
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: /node_modules/
+    }),
+    commonJs()
+  ],
+  external: [/@babel\/runtime/]
+}
