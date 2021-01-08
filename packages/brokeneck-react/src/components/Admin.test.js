fdc79458f4ce6599d0a3e4c6846f447ad22d6c16
Mathieu Davy <mathieu.davy@nearform.com>
2021-01-08 11:09:22 +0000

    feat: admin tests refactor

--- packages/brokeneck-react/src/components/Admin.test.js
+++ packages/brokeneck-react/src/components/Admin.test.js
@@ -2,11 +2,62 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { useQuery } from 'graphql-hooks'
+import deepmerge from 'deepmerge'
 
 import ThemeSwitcherProvider from '../components/ThemeSwitcherProvider'
 import mockRootContext from '../test-utils/mockRootContext'
+import { LOAD_ROOT } from '../graphql'
 
 import Admin from './Admin'
+
+const mockUseQuery = (overrides = {}) => query => {
+  let data = {}
+
+  if (query === LOAD_ROOT) {
+    data = mockRootContext
+  }
+
+  if (/query LoadUsers\(/.test(query)) {
+    data = {
+      users: { data: [], nextPage: '234567' }
+    }
+  }
+
+  if (/query LoadUser\(/.test(query)) {
+    data = {
+      user: {
+        Username: 'a_user',
+        Enabled: true,
+        UserStatus: 'FORCE_CHANGE_PASSWORD',
+        UserCreateDate: '2021-01-07T12:33:07.571Z',
+        UserLastModifiedDate: '2021-01-07T12:33:07.571Z',
+        groups: []
+      },
+      groups: { data: [], nextPage: '234567' }
+    }
+  }
+
+  if (/query LoadGroups\(/.test(query)) {
+    data = {
+      groups: { data: [], nextPage: '234567' }
+    }
+  }
+
+  if (/query LoadGroup\(/.test(query)) {
+    data = {
+      group: {
+        GroupName: 'AnotherGroup',
+        Description: 'Another group description',
+        CreationDate: '2021-01-05T10:42:20.733Z',
+        LastModifiedDate: '2021-01-05T10:42:20.733Z',
+        users: { data: [], nextPage: '234567' }
+      },
+      users: { data: [], nextPage: '234567' }
+    }
+  }
+
+  return deepmerge({ data, loading: false }, overrides)
+}
 
 jest.mock('graphql-hooks', () => {
   const originalLib = jest.requireActual('graphql-hooks')
@@ -20,10 +71,11 @@ jest.mock('graphql-hooks', () => {
 
 describe('Admin', () => {
   it('should render Admin component when loading', () => {
-    useQuery.mockImplementation(() => ({
-      data: { provider: { name: 'cognito' } },
-      loading: true
-    }))
+    useQuery.mockImplementation(
+      mockUseQuery({
+        loading: true
+      })
+    )
 
     render(
       <ThemeSwitcherProvider>
@@ -37,17 +89,7 @@ describe('Admin', () => {
   })
 
   it('should render Admin component when data has loaded for users', () => {
-    // This is dirty, as it returns a mix of responses for all queries
-    useQuery.mockImplementation(() => {
-      return {
-        data: {
-          provider: { name: 'cognito' },
-          users: { data: [], nextPage: '234567' },
-          ...mockRootContext
-        },
-        loading: false
-      }
-    })
+    useQuery.mockImplementation(mockUseQuery())
 
     render(
       <ThemeSwitcherProvider>
@@ -61,28 +103,7 @@ describe('Admin', () => {
   })
 
   it('should render Admin component when data has loaded for a single user', () => {
-    // This is dirty, as it returns a mix of responses for all queries
-    useQuery.mockImplementation(() => {
-      return {
-        data: {
-          provider: {
-            capabilities: { canSearchGroups: true },
-            name: 'cognito'
-          },
-          user: {
-            Username: 'a_user',
-            Enabled: true,
-            UserStatus: 'FORCE_CHANGE_PASSWORD',
-            UserCreateDate: '2021-01-07T12:33:07.571Z',
-            UserLastModifiedDate: '2021-01-07T12:33:07.571Z',
-            groups: []
-          },
-          groups: { data: [], nextPage: '234567' },
-          ...mockRootContext
-        },
-        loading: false
-      }
-    })
+    useQuery.mockImplementation(mockUseQuery())
 
     render(
       <ThemeSwitcherProvider>
@@ -96,20 +117,7 @@ describe('Admin', () => {
   })
 
   it('should render Admin component when data has loaded for groups', () => {
-    // This is dirty, as it returns a mix of responses for all queries
-    useQuery.mockImplementation(() => {
-      return {
-        data: {
-          provider: {
-            capabilities: { canSearchGroups: true },
-            name: 'cognito'
-          },
-          groups: { data: [], nextPage: '234567' },
-          ...mockRootContext
-        },
-        loading: false
-      }
-    })
+    useQuery.mockImplementation(mockUseQuery())
 
     render(
       <ThemeSwitcherProvider>
@@ -123,27 +131,7 @@ describe('Admin', () => {
   })
 
   it('should render Admin component when data has loaded for a single group', () => {
-    // This is dirty, as it returns a mix of responses for all queries
-    useQuery.mockImplementation(() => {
-      return {
-        data: {
-          provider: {
-            capabilities: { canSearchGroups: true },
-            name: 'cognito'
-          },
-          group: {
-            GroupName: 'AnotherGroup',
-            Description: 'Another group description',
-            CreationDate: '2021-01-05T10:42:20.733Z',
-            LastModifiedDate: '2021-01-05T10:42:20.733Z',
-            users: { data: [], nextPage: '234567' }
-          },
-          users: { data: [], nextPage: '234567' },
-          ...mockRootContext
-        },
-        loading: false
-      }
-    })
+    useQuery.mockImplementation(mockUseQuery())
 
     render(
       <ThemeSwitcherProvider>
