755e1297f89720e307904d1e7c49ad90d9f19a71
Peter <peterabsolon@yahoo.com>
2021-02-03 18:47:10 +0100

    chore: try with separate cache steps - should be fast now

--- packages/brokeneck-react/src/index.js
+++ packages/brokeneck-react/src/index.js
@@ -4,10 +4,10 @@ import { ClientContext } from 'graphql-hooks'
 import { BrowserRouter } from 'react-router-dom'
 
 import ThemeSwitcherProvider from './components/ThemeSwitcherProvider'
-import useGraphQLClient from './hooks/useGraphQLClient'
 import Admin from './components/Admin'
 import ErrorBoundary from './components/ErrorBoundary'
 import GraphQLErrorBoundary from './components/GraphQLErrorBoundary'
+import useGraphQLClient from './hooks/useGraphQLClient'
 
 export default function Main({ token, serverUrl, basename }) {
   const [client, GraphQLErrorContextProvider] = useGraphQLClient(
